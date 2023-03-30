09ca8a750fd6dc6b044a05c8e68c3b6faa9068a7
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-03-30 14:10:30 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Pham Nguyen Phu on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
