e64018676fd9f591495855dafdf0fd6212b58bde
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-03-31 14:20:25 +0700

    Develop the detail view for each animal with dynamic data - PART 1

---
+++ Africa/App/AnimalDetailView.swift
@@ -0,0 +1,76 @@
+//
+//  AnimalDetailView.swift
+//  Africa
+//
+//  Created by Pham Nguyen Phu on 31/03/2023.
+//
+
+import SwiftUI
+
+struct AnimalDetailView: View {
+    // MARK: - PROPERTY
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // HERO IMAGE
+                Image(animal.image)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(8)
+                
+                // TITLE
+                Text(animal.name.uppercased())
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.primary)
+                    .padding(.vertical, 8)
+                    .background(
+                        Color.accentColor
+                            .frame(height: 6)
+                            .offset(y: 24)
+                    )
+                
+                // HEADLINE
+                Text(animal.headline)
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal)
+                
+                // GALLERY`
+                
+                Group {
+                    HeadingView(headerImage: "photo.on.rectangle.angled", headerName: "Wilderness in Pictures")
+                    InsetGalleryView(animal: animal)
+                }
+                .padding(.horizontal)
+                
+                // FACTS
+                
+                // DESCRIPTION
+                
+                // MAP
+                
+                // LINK
+            } //: VSTACK
+        } //: SCROLLVIEW
+        .navigationTitle("Learn about \(animal.name)")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+// MARK: - PROPERTY
+
+struct AnimalDetailView_Previews: PreviewProvider {
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    static var previews: some View {
+        NavigationStack {
+            AnimalDetailView(animal: animals[0])
+        }
+    }
+}
--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTY
 
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+
     // MARK: - BODY
 
     var body: some View {
@@ -18,14 +20,22 @@ struct ContentView: View {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-                AnimalListView()
-            }
+
+                ForEach(animals) { item in
+                    NavigationLink {
+                        AnimalDetailView(animal: item)
+                    } label: {
+                        AnimalListItemView(animal: item)
+                    } //: NAVIGION LINK
+                    .padding(.vertical, 4)
+                } //: FOREACH LOOP
+            } //: LIST
             .listStyle(.plain)
             .navigationTitle("Africa")
             .navigationBarTitleDisplayMode(.large)
         } detail: {
             EmptyView()
-        }
+        } //: NAVIGATION SPLIT VIEW
     }
 }
 
