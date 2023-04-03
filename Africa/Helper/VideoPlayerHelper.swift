5597104c05b700972b0c676830dce1c880ee45d5
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-04-03 14:19:30 +0700

    create a Video Player with SwiftUI framework - Part 2

---
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by Pham Nguyen Phu on 03/04/2023.
+//
+
+import AVKit
+import Foundation
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    return videoPlayer!
+}
