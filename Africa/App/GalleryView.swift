ca016fd927a0197035cfebfb238ea828a8092c71
Pham Nguyen Phu <57014052+phamnguyenphu@users.noreply.github.com>
2023-04-04 13:57:39 +0700

    Update GalleryView.swift

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,9 +8,83 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTY
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    // SIMPLE GRID DEFINITION
+    //    let gridLayout: [GridItem] = [
+    //        GridItem(.flexible()),
+    //        GridItem(.flexible()),
+    //        GridItem(.flexible())
+    //    ]
+    
+    // EFFICIENT GRID DEFINITION
+    //    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    // DYNAMIC GRID DEFINITION
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: GridItem(.flexible()), count: Int(gridColumn))
+    }
+    
+    // MARK: - BODY
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery View")
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay {
+                        Circle()
+                            .stroke(Color.white, lineWidth: 8)
+                    }
+                
+                // MARK: - SLIDER
+                
+                Slider(value: $gridColumn, in: 2 ... 4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn) { _ in
+                        withAnimation(.easeIn) {
+                            gridSwitch()
+                        }
+                    }
+                
+                // MARK: - GRID
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 20, content: {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay {
+                                Circle()
+                                    .stroke(Color.white, lineWidth: 2)
+                            }
+                            .onTapGesture {
+                                withAnimation(.easeOut) {
+                                    selectedAnimal = item.image
+                                    haptics.impactOccurred()
+                                }
+                            }
+                    } //: FOREACH LOOP
+                }) //: LazyVGrid
+                .padding(.horizontal, 10)
+                .padding(.vertical, 50)
+                .onAppear {
+                    gridSwitch()
+                }
+            } //: VSTACK
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background {
@@ -18,6 +92,8 @@ struct GalleryView: View {
         }
     }
 }
+
+// MARK: - PREVIEW
 
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
